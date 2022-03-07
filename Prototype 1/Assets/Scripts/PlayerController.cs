ba579d7d9d2117f0c665a78fc75c3dd7c8135ff0
isram8 <israfilrules@live.com>
2022-03-07 19:13:06 +0200

    Changed player movement from using transform.translate to rigidbody forces

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float horizontalInput;
     [SerializeField] public float speed;
     [SerializeField] public float turnSpeed;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,11 +21,14 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxisRaw("Vertical");
         horizontalInput = Input.GetAxisRaw("Horizontal");
 
+        
     }
 
     private void FixedUpdate()
     {
-        transform.Translate(0, 0, verticalInput * speed * Time.deltaTime);
+        
+        rb.AddForce(Vector3.forward * verticalInput * speed, ForceMode.Acceleration);
+
         transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
 
         

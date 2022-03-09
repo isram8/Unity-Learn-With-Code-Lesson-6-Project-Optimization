1b6e11d9bf1262beee6fdc409ea3f504321e7f93
isram8 <israfilrules@live.com>
2022-03-09 11:07:36 +0200

    Fixed car driving in mid-air, added km/h and rpm display, fine tuned speed and car feel

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float verticalInput;
     [SerializeField] float horizontalInput;
+    [SerializeField] float speed;
+    [SerializeField] float rpm;
     [SerializeField] public float horsePower;
     [SerializeField] public float turnSpeed;
     [SerializeField] GameObject centerOfMass;
+    public GameObject ground;
+    public TextMeshProUGUI speedometerText;
+    public TextMeshProUGUI rpmText;
     Rigidbody rb;
+
+    const float wheelRadius = 37;
+    const float wheelCircumference = wheelRadius * 2 * 3.14f;
+
+    [SerializeField] bool isOnGround;
 
     private void Start()
     {
@@ -28,11 +39,41 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (isOnGround && verticalInput != 0)
+        {
+            rb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
         
-        rb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
-        
+        }
+
+        speed = Mathf.Round(rb.velocity.magnitude * 3.6f);
+        rpm = Mathf.Round((speed % 30) * 40);
+
+        speedometerText.text = "Km/h: " + speed.ToString();
+        rpmText.text = "RPM: " + rpm.ToString();
+
         transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
 
-        
+
+
+
+
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isOnGround = false;
+
+        }
     }
 }
